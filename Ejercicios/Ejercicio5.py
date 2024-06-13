eb0740eb227523b800281fd215864b1f378805f3
unknown <Jorgemathez14@gmail.com>
2024-06-13 17:42:28 -0300

    Ejercicio Operadores

--- Ejercicios/Ejercicio5.py
+++
@@ -1,9 +0,0 @@
-# Tipos booleanos (bool)
-miBooleano = 1 > 2
-print(miBooleano)
-
-if miBooleano: 
-    print("El resultado es Verdadero")
-else:
-        print("El resultado es Falso")
-
---
+++ Ejercicios/Operadores.py
@@ -0,0 +1,25 @@
+# Operadores aritmeticos
+operandoA = 8
+OperandoB = 5
+
+suma = operandoA + OperandoB
+print(f"resultado de la suma: {suma}")
+print(f"El resultado de la suma es: {suma}")
+
+resta = operandoA - OperandoB
+print(f"El resultado de la resta es: {resta}")
+
+multiplicacion = operandoA * OperandoB
+print(f"El resultado de la multiplicacion es: {multiplicacion}")
+
+Division = operandoA / OperandoB
+print(f"El resultado de la Division es: {Division}")
+
+Division = operandoA // OperandoB
+print(f"El resultado de la Division int es: {Division}")
+
+Modulo = operandoA % OperandoB
+print(f"El resultado del Modulo es: {Modulo}")
+
+exponente = operandoA ** OperandoB
+print(f"El resultado del exponente es: {exponente}")
